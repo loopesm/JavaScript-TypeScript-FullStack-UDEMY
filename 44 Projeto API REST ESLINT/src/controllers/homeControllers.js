01cd460d95a533a0e43a57499aa3c63fb44fcdaa
Moises Lopes <moiseslopes999@gmail.com>
2025-12-30 21:38:44 -0300

    API Pronta, enviando daods para a base de dados

--- 44 Projeto API REST ESLINT/src/controllers/homeControllers.js
+++ 44 Projeto API REST ESLINT/src/controllers/homeControllers.js
@@ -1,8 +1,20 @@
+import Aluno from '../models/AlunoModels.js';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
-    });
+  async index(req, res) {
+    try {
+      const novoALuno = await Aluno.create({
+        nome: 'João Silva',
+        sobrenome: 'Souza',
+        email: 'H0B2K@example.com',
+        idade: 20,
+        peso: 70.5,
+        altura: 1.75,
+      });
+      res.json(novoALuno);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
